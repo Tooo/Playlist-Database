0450cdebe752d186d0a703ea372261034f11664d
Travis <34826963+Toooo123@users.noreply.github.com>
2021-03-16 11:52:55 -0700

    Playlist Classes

---
+++ Playlist.py
@@ -0,0 +1,17 @@
+class Playlist:
+    def __init__(self, name, username, date):
+        self.name = name
+        self.username = username
+        self.date = date
+
+
+class PublicPlaylist(Playlist):
+    def __init__(self, name, username, date):
+        super().__init__(name, username, date)
+
+
+class PrivatePlaylist(Playlist):
+    def __init__(self, name, username, date, password):
+        super().__init__(name, username, date)
+        self.password
+        
