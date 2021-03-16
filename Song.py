7b34a7367216ecf6ed5714df7c4c186cd0611de3
Travis <34826963+Toooo123@users.noreply.github.com>
2021-03-16 12:06:40 -0700

    Song Class

---
+++ Song.py
@@ -0,0 +1,13 @@
+class Song:
+    genres = []
+
+    def __init__(self, songID, name, artist, duration):
+        self.songID = songID
+        self.name = name
+        self.artist = artist
+        self.duration = duration
+
+class SongGenre:
+    def __init__(self, songID, genre):
+        self.songID = songID
+        self.genre = genre
