c39fe3635bf425bc8df5de567889fed21617e243
Travis <34826963+Toooo123@users.noreply.github.com>
2021-03-16 14:28:06 -0700

    sample main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-import Song
-import User
-import Playlist
-import Relations
+from song import *
+from playlist import *
+from user import *
+from relations import *
 
+def introduction_prints():
+    print("** Playlist Database **")
+    print("1 - Login")
 
+introduction_prints()
 
+input1 = input("> ")
+
+if (input1 == "1"):
+    username = input("Enter Username: ")
+    user = User(username)
+    print(user.username + " has been created.")
