e49c5bae52aeae43ee89d39a116cfc87ac9aa1cf
Travis <34826963+Toooo123@users.noreply.github.com>
2021-03-16 12:08:40 -0700

    User Class

---
+++ User.py
@@ -0,0 +1,10 @@
+class User:
+    genres = []
+    
+    def __init__(self, username):
+        self.username = username
+
+class UserGenre:
+    def __init__(self, username, genre):
+        self.username = username
+        self.genre = genre
