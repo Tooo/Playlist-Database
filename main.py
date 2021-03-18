dfed490250c59efe233863d8559c364ab2632c84
Travis <34826963+Toooo123@users.noreply.github.com>
2021-03-17 17:11:13 -0700

    testing database

--- main.py
+++ main.py
@@ -2,10 +2,35 @@ from song import *
 from playlist import *
 from user import *
 from relations import *
+import mysql.connector
 
 def introduction_prints():
     print("** Playlist Database **")
     print("1 - Login")
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="user",
+    password="password"
+)
+
+cursor = db.cursor()
+cursor.execute("CREATE DATABASE IF NOT EXISTS playlistdatabase")
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="user",
+    password="password",
+    database="playlistdatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE IF NOT EXISTS user (username VARCHAR(255))")
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+    print(x)
 
 introduction_prints()
 
