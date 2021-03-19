647d4de7fb0eb1907b8032a9ffb4ffbae51b3339
Travis <34826963+Toooo123@users.noreply.github.com>
2021-03-18 22:24:31 -0700

    basic login

--- webui.py
+++ webui.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import *
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def login():
+   return render_template('login.html')
+
+@app.route('/main', methods = ['POST', 'GET'])
+def main():
+    if request.method == 'POST':
+        result = request.form
+        return render_template("main.html", result = result)
 
 if __name__ == '__main__':
     app.run()
